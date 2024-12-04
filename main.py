d3f750056ccb83f7e82c02ef70e02bab13080052
VamanR06 <131827751+VamanR06@users.noreply.github.com>
2024-12-04 01:45:21 -0500

    Added solver
    Testing in main.py
    Need to integrate with camera capture system

--- main.py
+++ main.py
@@ -1,56 +1,40 @@
-import kociemba
+from solver import Cube
 
+solved = {
+    "Up": ["orange", "orange", "orange", "orange", "orange", "orange", "orange", "orange", "orange"],
+    "Down": ["red", "red", "red", "red", "red", "red", "red", "red", "red"],
+    "Front": ["green", "green", "green", "green", "green", "green", "green", "green", "green"],
+    "Right": ["white", "white", "white", "white", "white", "white", "white", "white", "white"],
+    "Back": ["blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue", "blue"],
+    "Left": ["yellow", "yellow", "yellow", "yellow", "yellow", "yellow", "yellow", "yellow", "yellow"]
+}
 
+u_state = {
+    "Up": ["orange", "orange", "orange", "orange", "orange", "orange", "orange", "orange", "orange"],
+    "Down": ["red", "red", "red", "red", "red", "red", "red", "red", "red"],
+    "Front": ["yellow", "yellow", "yellow", "green", "green", "green", "green", "green", "green"],
+    "Right": ["green", "green", "green", "white", "white", "white", "white", "white", "white"],
+    "Back": ["white", "white", "white", "blue", "blue", "blue", "blue", "blue", "blue"],
+    "Left": ["blue", "blue", "blue", "yellow", "yellow", "yellow", "yellow", "yellow", "yellow"]
+}
 
+u_r_state = {
+    "Up": ["blue", "blue", "blue", "orange", "orange", "orange", "orange", "orange", "orange"],
+    "Down": ["red", "red", "green", "red", "red", "green", "red", "red", "green"],
+    "Front": ["yellow", "yellow", "yellow", "green", "green", "orange", "green", "green", "orange"],
+    "Right": ["green", "green", "orange", "white", "white", "white", "white", "white", "white"],
+    "Back": ["white", "white", "white", "red", "blue", "blue", "red", "blue", "blue"],
+    "Left": ["red", "blue", "blue", "yellow", "yellow", "yellow", "yellow", "yellow", "yellow"]
+}
 
+random_state = {
+    "Up": ["green", "yellow", "orange", "green", "orange", "white", "yellow", "blue", "yellow"],
+    "Down": ["white", "white", "white", "orange", "red", "red", "blue", "blue", "red"],
+    "Front": ["blue", "white", "blue", "blue", "green", "green", "orange", "orange", "orange"],
+    "Right": ["red", "green", "yellow", "orange", "white", "green", "blue", "white", "yellow"],
+    "Back": ["green", "red", "red", "red", "blue", "blue", "green", "red", "red"],
+    "Left": ["white", "yellow", "orange", "orange", "yellow", "yellow", "white", "yellow", "green"]
+}
 
-''''
-             |------------|
-             |-U1--U2--U3-|
-             |------------|
-             |-U4--U5--U6-|
-             |------------|
-             |-U7--U8--U9-|
-|------------|------------|------------|------------|
-|-L1--L2--L3-|-F1--F2--F3-|-R1--R2--R3-|-B1--B2--B3-|
-|------------|------------|------------|------------|
-|-L4--L5--L6-|-F4--F5--F6-|-R4--R5--R6-|-B4--B5--B6-|
-|------------|------------|------------|------------|
-|-L7--L8--L9-|-F7--F8--F9-|-R7--R8--R9-|-B7--B8--B9-|
-|------------|------------|------------|------------|
-             |-D1--D2--D3-|
-             |------------|
-             |-D4--D5--D6-|
-             |------------|
-             |-D7--D8--D9-|
-             |------------|
-
-             
-U - Orange
-L - White
-F - Green
-R - Yellow
-B - Blue
-D - Red
-
-
-Ask user to take pictures of each face based on the centers, 
-since centers don't change; makes mapping easier
-
-Map each face color to the corresponding ULFRBD value
-
-Systems:
-
-- mapping system (image color to location of face)
-- image processor (take photo and extract color data)
-- pass to k solver
-- output graphics
-
-
-'''
-
-
-
-print(kociemba.solve('UUUUUUUUUFFFRRRRRRLLLFFFFFFDDDDDDDDDBBBLLLLLLRRRBBBBBB'))
-
-# U R F D L B - Order to type pieces out for a scramble string
+cube = Cube(random_state)
+print(cube.solve())
